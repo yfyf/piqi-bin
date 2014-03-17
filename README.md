579867226e9a1857562b3db5a93abfdd888213b5
Ignas Vyšniauskas <baliulia@gmail.com>
2014-03-17 13:12:49 +0200

    Initial super-alpha version

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 piqi-bin
 ========
 
-This is a dummy rebar dependency which copies over the piqi binary from your
-system (based on PATH) and stores in its `priv/` dir for later access.
+This is a dummy / wrapper rebar dependency which copies over the piqi binary
+from your system (based on PATH) and stores in its `priv/` dir for later
+access.
 
-It also verifies the version.
+It also verifies the version. See `rebar.config` for details.
+
+This can be used to ensure, that you will always use the same piqi version at
+runtime as you did at buildtime.
+
+WARNING: largely untested at this point, especially the version contraint part.
---
+++ plugins/piqi_bin_plugin.erl
@@ -0,0 +1,95 @@
+-module(piqi_bin_plugin).
+
+-export([
+    'post_compile'/2
+]).
+
+%% ====================================================================
+%% rebar plugin hooks
+%% ====================================================================
+
+'post_compile'(Config, _AppFile) ->
+    verify_piqi_version(Config).
+
+%% ====================================================================
+%% Private parts
+%% ====================================================================
+
+
+verify_piqi_version(Config) ->
+    ReqPiqiVsn = rebar_config:get(Config, req_piqi_vsn, undefined),
+    case ReqPiqiVsn of
+        undefined ->
+            ok;
+        _ ->
+            VsnOld = os:cmd("piqi version"), %% returned by piqi =< 0.6.5
+            VsnNew = os:cmd("piqi --version"), %% returned by piqi > 0.6.5
+            StrVsn = case VsnOld of
+                "0." ++ _ -> VsnOld;
+                _ -> VsnNew
+            end,
+            Vsn = parse_vsn(StrVsn),
+            compatible_vsns(Vsn, ReqPiqiVsn)
+    end.
+
+compatible_vsns(Vsn, []) ->
+    ok;
+compatible_vsns(Vsn, [{Cmp, ReqVsnStr}|ReqVsns]) ->
+    ReqVsn = parse_vsn(ReqVsnStr),
+    case compatible_vsn(Cmp, Vsn, ReqVsn) of
+        true ->
+            compatible_vsns(Vsn, ReqVsns);
+        false ->
+            rebar_utils:abort(
+              "Piqi version ~p incompatible. Could not satisfy constraint ~p~n",
+              [Vsn, {Cmp, ReqVsnStr}]
+            )
+    end.
+
+cmp(X, Y) when X > Y ->
+    gt;
+cmp(X, X) ->
+    eq;
+cmp(X, Y) ->
+    lt.
+
+compatible_vsn(Cmp, [X], [Y]) when is_integer(X), is_integer(Y) ->
+    cmp(X, Y) == Cmp;
+compatible_vsn(Cmp, [X|Xs], [Y|Ys]) when is_integer(X), is_integer(Y) ->
+    case cmp(X, Y) of
+        eq ->
+            compatible_vsn(Cmp, Xs, Ys);
+        Cmp ->
+            true;
+        _ ->
+            false
+    end;
+compatible_vsn(Cmp, [X|Xs], [Y|Ys]) when is_integer(X) ->
+    true;
+compatible_vsn(Cmp, [X|Xs], [Y|Ys]) ->
+    cmp(X, Y) == Cmp;
+compatible_vsn(Cmp, [], [_|_]) ->
+    Cmp == lt;
+compatible_vsn(Cmp, _, []) ->
+    Cmp == gt.
+
+parse_vsn(StringVsn) ->
+     try
+         Tokens = string:tokens(string:strip(StringVsn, right, $\n), ".-+"),
+         lists:map(fun (Token) ->
+                case string:to_integer(Token) of
+                    {error, _} ->
+                        Token;
+                    {Int, []} ->
+                        Int;
+                    _ ->
+                        Token
+                end
+            end,
+            Tokens
+        )
+     catch
+         _:_ ->
+             rebar_utils:abort("Error parsing piqi version: ~p~n",
+                               [StringVsn])
+     end.
